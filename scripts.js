0fca945e58c4677dea9550a48c78f7e10c9f4927
Eric Schneider <esschneider6@gmail.com>
2020-09-23 11:01:23 -0400

    Adding scripts tab for jquery

---
+++ scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".dropButton2").click(function(){
+        $(".body-text1").slideDown();
+    });
+    $(".dropButton1").click(function(){
+        $(".body-text1").slideUp();
+    });  
+  });
